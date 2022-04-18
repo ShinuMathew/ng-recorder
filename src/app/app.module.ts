adae7d2e5fa79f0884b09d9174b1257df1f84c99
Shinu Mathew <shinumathew@Shinus-MacBook-Pro.local>
2022-04-18 17:13:06 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideojsRecordComponent } from './videojs-record/videojs-record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojsRecordComponent
   ],
   imports: [
     BrowserModule,
